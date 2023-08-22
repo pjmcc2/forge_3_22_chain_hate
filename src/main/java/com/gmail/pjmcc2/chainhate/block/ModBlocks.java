d93703f224352103ab247bbf157fb264f35e66c4
Peter <pjmcc2@gmail.com>
2023-08-22 10:43:22 -0700

    Several changes, additions, and fixes.

--- src/main/java/com/gmail/pjmcc2/chainhate/block/ModBlocks.java
+++ src/main/java/com/gmail/pjmcc2/chainhate/block/ModBlocks.java
@@ -18,8 +18,8 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, ChainHate.MOD_ID);
 
-    public static final RegistryObject<Block> ITEM_DISASSEMBLER = registerBlock("item_disassembler",
-            () -> new Block(BlockBehaviour.Properties.of()))
+    // public static final RegistryObject<Block> ITEM_DISASSEMBLER = registerBlock("item_disassembler",
+         //   () -> new Block(BlockBehaviour.Properties.of()))
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
                                                                      CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/com/gmail/pjmcc2/chainhate/item/ModItems.java
+++ src/main/java/com/gmail/pjmcc2/chainhate/item/ModItems.java
@@ -1,2 +1,21 @@
-package com.gmail.pjmcc2.chainhate.item;public class ModItems {
+package com.gmail.pjmcc2.chainhate.item;
+
+import com.gmail.pjmcc2.chainhate.ChainHate;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ChainHate.MOD_ID);
+
+    public static final RegistryObject<Item> CHAIN_LINK = ITEMS.register("chain_link",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
